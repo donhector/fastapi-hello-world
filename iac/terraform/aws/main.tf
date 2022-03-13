1f562d5acddb01138bc024470c35e1cfacc4ce26
Hector Molina <2475379+donhector@users.noreply.github.com>
2022-03-13 01:41:58 +0100

    Fixes in IaC files and workflow

--- iac/terraform/aws/main.tf
+++ iac/terraform/aws/main.tf
@@ -1,9 +1,4 @@
 provider "aws" {
-  profile = var.profile
-  region  = var.region
-}
-
-provider "aws" {
   alias      = "plain_text_access_keys_provider"
   region     = "us-west-1"
   access_key = "AKIAIOSFODNN7EXAMPLE"
