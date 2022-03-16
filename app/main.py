bf2c50549742707841f00f3faf5b0823bb57aa30
Hector Molina <2475379+donhector@users.noreply.github.com>
2022-03-16 16:17:18 +0100

    Resolves zap nosniff issue

--- app/main.py
+++ app/main.py
@@ -1,11 +1,11 @@
 import fastapi
 
-from fastapi import FastAPI
+from fastapi import FastAPI, Response
 
 app = FastAPI()
 
 
 @app.get("/")
-async def root():
-    return {"message": "Hello World"}
-
+async def root(response: Response):
+  response.headers["X-Content-Type-Options"] = "nosniff"
+  return {"message": "Hello World"}
